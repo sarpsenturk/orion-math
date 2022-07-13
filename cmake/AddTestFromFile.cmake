e9f45ef00263bb8e862e9157bc999ec8fbb97836
sarpsenturk <sarpsenturl38@gmail.com>
2022-07-13 16:52:11 +0300

    Fix submodule situation

--- cmake/AddTestFromFile.cmake
+++
@@ -1,5 +0,0 @@
-function(add_test_from_file filename project_lib)
-    add_executable(${filename} ${filename}.cpp)
-    target_link_libraries(${filename} PRIVATE ${project_lib} gtest_main)
-    gtest_discover_tests(${filename})
-endfunction()
